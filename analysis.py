49fbf2a79e4d8f2099fefdfc406e16abbb6aee88
David Kurka <david.kurka@gmail.com>
2014-06-08 23:48:03 -0300

    working on analysis

--- analysis.py
+++ analysis.py
@@ -4,6 +4,7 @@ import json
 import numpy as np
 import pickle
 from PersistencyLayer import TweetsPersister
+import copy
 
 ###############################################################################
 #PREPARACAO DOS DADOS
@@ -17,13 +18,14 @@ persister = TweetsPersister()
 #ROOTUSER = 14594813
 #root_tweets = persister.loadTweetsOfUser(ROOTUSER) #get all root tweets
 
-
-f = open("folha2600classes.data", "r") #load tweets already categorized by autoclassifier
+#load tweets already categorized by autoclassifier
+f = open("folha2600classes.data", "r") 
 root_tweets_extended = pickle.load(f)
 f.close()
 
 
-class_dict = {'cotidiano':1, 'esporte':2, 'mundo':3, 'poder':4, 'ilustrada':5, 'mercado':6}
+class_dict = {'cotidiano':1, 'esporte':2, 'mundo':3, 
+              'poder':4, 'ilustrada':5, 'mercado':6}
 
 def topiccleaner(tweet):
     if tweet['class'] in class_dict.keys():
@@ -44,8 +46,8 @@ min_retweets = 20
 for root_tweet in root_tweets:
     retweets = persister.loadRetweets(root_tweet['tweet']['tweet_id'])
     lens.append(len(retweets))
-    if retweets and len(retweets)>=min_retweets: #limiting just stories with more than min_retweets
-        #retweets.insert(0, root_tweet) #insert root tweet at the beggining of the list
+    #limiting just stories with more than min_retweets
+    if retweets and len(retweets)>=min_retweets: 
         tweets_collection.append(retweets)
         classes.append(root_tweet['class'])
 
@@ -70,8 +72,9 @@ for col_num, col in enumerate(tweets_collection):
     tweets_matrix[col_indexes, col_num] = 1
     
 #remove bots from matrix
+#considera quem retweetou mais que 80% dos tweets um bot
 rsum = np.sum(tweets_matrix, axis=1)
-bots = np.where(rsum > 0.8*len(tweets_collection))[0] #considera quem retweetou mais que 80% dos tweets um bot
+bots = np.where(rsum > 0.8*len(tweets_collection))[0] 
 #TODO: melhorar definicao de bots. Talvez ver por timestamp
 
 
@@ -127,9 +130,7 @@ TM_dict['TM_pop'] = {'tweets_matrix':tweets_matrix.copy(),
 #3- Monta grafo da rede
 
 def buildgraph(nodes_list):
-
     G = nx.DiGraph()   
-
     edges = []
     total = float(len(nodes_list))
     for count, n in enumerate(nodes_list):
@@ -145,8 +146,7 @@ def buildgraph(nodes_list):
              edges.append((n,nei))
           #dirty_edges.append((n, nei))
        f.close()
-       #GDirty.add_edges_from(dirty_edges)
-    
+       #GDirty.add_edges_from(dirty_edges)   
     G.add_edges_from(edges)
     return G
 
@@ -188,8 +188,8 @@ removeIsolated(TM_dict['TM_pop'], G_pop)
 ###############################################################################
 #4- propagacao!
 
-TM_dict['TM_sparse_prop'] = TM_dict['TM_sparse'].copy()
-TM_dict['TM_pop_prop'] = TM_dict['TM_pop'].copy()
+TM_dict['TM_sparse_prop'] = copy.deepcopy(TM_dict['TM_sparse'])
+TM_dict['TM_pop_prop'] = copy.deepcopy(TM_dict['TM_pop'])
     
 def propagate(users, user, graph, tweet, visited):
     #visited.append(user)    
@@ -215,10 +215,60 @@ def propagate_tweetsmatrix(tm_dict, G):
     return tm_dict['tweets_matrix']
    
 
-TM_dict['TM_sparse_prop'] = propagate_tweetsmatrix(TM_dict['TM_sparse_prop'], G_sparse)
-TM_dict['TM_pop_prop'] = propagate_tweetsmatrix(TM_dict['TM_pop_prop'], G_pop)
+TM_dict['TM_sparse_prop']['tweets_matrix'] = propagate_tweetsmatrix(TM_dict['TM_sparse_prop'], G_sparse)
+TM_dict['TM_pop_prop']['tweets_matrix'] = propagate_tweetsmatrix(TM_dict['TM_pop_prop'], G_pop)
+
+###############################################################################
+#deteccao de comunidades
+import sys
+sys.path.insert(0, 'louvain')
+import community
 
 
+TM_dict['TM_sparse_com'] = copy.deepcopy(TM_dict['TM_sparse'])
+TM_dict['TM_pop_com'] = copy.deepcopy(TM_dict['TM_pop'])
+
+def community_matrix(tm_dict, G):
+    
+    tweets_matrix = tm_dict['tweets_matrix']    
+    G_un = G.to_undirected()
+    #first compute the best partition
+    partition = community.best_partition(G_un)
+    
+    
+    #agrupar as linhas!
+    community_matrix = np.zeros((len(set(partition.values())), len(tm_dict['tweets_collection']))) #empty matrix
+    
+    
+    #jeito 1: soma os valores de toda a comunidade
+    row_num = 0
+    for com in set(partition.values()):
+        list_nodes = [node for node in partition.keys()
+                                    if partition[node] == com]
+        row_indexes = np.array([int(np.where(tm_dict['users'] == user)[0]) for user in list_nodes]) 
+        print len(list_nodes) #get the size of the list of nodes
+        community_matrix[row_num, :] = tweets_matrix[row_indexes, :].sum(0)
+        row_num += 1
+    
+    #similaridade entre comunidades
+    
+    comsizes = np.bincount(partition.values())
+    for com in set(partition.values()):
+        toptweets = np.argsort(community_matrix[com,:])[::-1][:10]
+        if comsizes[com] > 10:
+            print "particao", com, comsizes[com]
+            for t in toptweets:
+                if community_matrix[com,t] > 0:
+                    print community_matrix[com, t]/comsizes[com], \
+                          sum(tweets_matrix[:,t])/float(tweets_matrix.shape[0]), \
+                          tm_dict['tweets_collection'][t][0]['text'][14:] 
+            print "\n"
+            
+    return community_matrix
+
+
+TM_dict['TM_sparse_com']['tweets_matrix'] = community_matrix(TM_dict['TM_sparse_prop'], G_sparse)
+TM_dict['TM_pop_com']['tweets_matrix'] = community_matrix(TM_dict['TM_pop_prop'], G_pop)
 
 
 #salva tudo
@@ -247,6 +297,7 @@ f.close()
 import random
 from scipy.spatial.distance import cdist
 from operator import itemgetter
+import matplotlib.pyplot as plt
 
 #KNN
     
@@ -258,10 +309,12 @@ n = 300 #number of samples
 samplei = random.sample(xrange(TM_dict['TM_pop']['tweets_matrix'].shape[1]), n) #select a group of tweets ids as sample
 
 samples = {}
-for key in TM_dict.keys():
+keys = TM_dict.keys()
+
+for key in keys:
     samples[key] = TM_dict[key]['tweets_matrix'][:,samplei]
 D = {}
-for key in TM_dict.keys():
+for key in keys:
     D[key] = cdist(TM_dict[key]['tweets_matrix'].T, samples[key].T, metric="jaccard")
 
 
@@ -280,12 +333,11 @@ for key in TM_dict.keys():
 
 tamanhos = [1, 3, 5, 7, 11, 25, 40, 50]
 resultados = np.zeros((len(TM_dict)+1, len(tamanhos)))
-Dkeys = D.keys()
 
 for i in range(n):
 
     #aplica knn para cada matriz
-    for j, key in enumerate(Dkeys):
+    for j, key in enumerate(keys):
         if i == 0:
             print j, key
         classes = TM_dict[key]['classes']
@@ -299,8 +351,8 @@ for i in range(n):
     
     #faz teste nulo, para comparacao
     classes = TM_dict['TM_sparse']['classes']
-    gabarito = classes[samplei[i]]              
-    vizinhos_rand = np.array(random.sample(range(len(vizinhos_sort)), len(vizinhos_sort)))
+    gabarito = classes[samplei[i]]         
+    vizinhos_rand = np.random.permutation(vizinhos_sort)
     for k in range(len(tamanhos)):
         kvizinhos = list(classes[vizinhos_rand[1:(tamanhos[k]+1)]])
         contagem = [(kvizinhos.count(v), v) for v in set(kvizinhos)]
@@ -311,11 +363,78 @@ print resultados
         
 for i, tam in enumerate(tamanhos):
     print "K =", tam
-    for j, nome in enumerate(TM_dict.keys()):
+    for j, nome in enumerate(keys):
         print nome, ":\t", resultados[j,i]/float(n)
         
     print "random :", resultados[-1, i]/float(n)
     print   
+
+
+###############################################################################
+#regressao linear e SVM
+from sklearn import svm
+
+def prepare_sets(tm_dict, split=0.5):
+    
+    class_dict = {'cotidiano':1, 'esporte':2, 'mundo':3, 
+              'poder':4, 'ilustrada':5, 'mercado':6}    
+    print tm_dict['classes'][:10]
+    Y = np.array([class_dict[classe] for classe in tm_dict['classes']])
+#    Y_rand = list(Y)
+#    random.shuffle(Y_rand)
+#    Y_rand = np.array(Y_rand) #TODO: comparar com nulo
+    
+    X = tm_dict['tweets_matrix'].T
+    
+    
+    total_size = Y.size
+    random_indices = np.random.permutation(total_size)
+    
+    
+    train_limit = split
+    Y_train = Y[random_indices[:int(train_limit * total_size)]]
+    X_train = X[random_indices[:int(train_limit * total_size)]]
+    
+    Y_test = Y[random_indices[int(train_limit * total_size):]]
+    X_test = X[random_indices[int(train_limit * total_size):]]
+    
+    return (X_train, Y_train, X_test, Y_test)
+
+def linear_reg(tm_dict):
+    (X_train, Y_train, X_test, Y_test) = prepare_sets(tm_dict)
+    
+    teta = np.dot(np.linalg.pinv(X_train), Y_train)
+
+    Y_l = np.around(np.dot(X_train, teta))
+    print "acertos treino:", sum(Y_train == Y_l) / float(len(Y_l))
+        
+    Y_l = np.around(np.dot(X_test, teta))    
+    print "acertos teste:", sum(Y_test == Y_l) / float(len(Y_l))
+
+
+
+#svm
+def svc(tm_dict):
+    (X_train, Y_train, X_test, Y_test) = prepare_sets(tm_dict)
+    classificador = svm.SVC(kernel='linear')
+    classificador.fit(X_train, Y_train)
+    
+    Y_class = classificador.predict(X_train)
+    acertos = sum(Y_train == Y_class) / float(len(Y_class))
+    print "acertos treino:", acertos 
+
+   
+    Y_classificado = classificador.predict(X_test)
+    acertos = sum(Y_test == Y_classificado) / float(len(Y_classificado))
+    print "acertos teste:", acertos
+    
+
+for (key,item) in TM_dict.itervalues():
+    print key
+    svc(item)
+
+
+#TODO: LDA
 
 
 
@@ -335,14 +454,16 @@ for key in TM_dict.keys():
     centres, xtoc, dist = kmeans(X, ncluster, initcentres=None, metric="cosine", maxiter=20, delta=.00001, verbose=True)
 
    
-#    #####
-#    #imprime clusters
-#    cols = []
-#    for j in range(ncluster):
-#        for i in np.where(xtoc == j)[0]:
-#            print classes[i], "\t", tweets_collection[i][0]['text'][14:] 
-#            cols.append(i)
-#        print "\n\n"
+   
+tweets_collection = TM_dict[key]['tweets_collection']   
+#####
+#imprime clusters
+cols = []
+for j in range(ncluster):
+    for i in np.where(xtoc == j)[0]:
+        print classes[i], "\t", tweets_collection[i][0]['text'][14:] 
+        cols.append(i)
+    print "\n\n"
 
 
 #plt.matshow(X, cmap=plt.cm.Blues)
@@ -352,178 +473,40 @@ for key in TM_dict.keys():
 #plt.title("clustered dataset")
 
         
+    
 
-
-
+        
 ###############################################################################
-#deteccao de comunidades!
-import sys
-sys.path.insert(0, 'louvain')
-import community
-import matplotlib.pyplot as plt
+#biclusterizacao (inclose)
 
-G = G_sparse.to_undirected()
-#first compute the best partition
-partition = community.best_partition(G)
+#inclose
 
 
-#agrupar as linhas!
-community_matrix = np.zeros((len(set(partition.values())), len(tweets_collection))) #empty matrix
 
-
-#jeito 1: soma os valores de toda a comunidade
-row_num = 0
-for com in set(partition.values()):
-    list_nodes = [node for node in partition.keys()
-                                if partition[node] == com]
-    row_indexes = np.array([int(np.where(users == user)[0]) for user in list_nodes]) 
-    print len(list_nodes) #get the size of the list of nodes
-    community_matrix[row_num, :] = tweets_matrix[row_indexes, :].sum(0)
-    row_num += 1
+for tm_dict in [TM_dict['TM_sparse'], TM_dict['TM_pop']]:
+    #1-roda inclose usando octave
+    import scipy.io as sio
+    import os
+    sio.savemat('matlab/twitter.mat', {'tweets_matrix':tm_dict['tweets_matrix']})
+    print "executando inclose. Isso pode demorar um pouco"
+    os.system("octave twitter-inclose.m")
+    mat_contents = sio.loadmat('matlab/bicl.mat')
+    biclusters = mat_contents['biclusters'][0]
     
     
-comsizes = np.bincount(partition.values())
-for com in set(partition.values()):
-    toptweets = np.argsort(community_matrix[com,:])[::-1][:10]
-    if comsizes[com] > 10:
-        print "particao", com, comsizes[com]
-        for t in toptweets:
-            if community_matrix[com,t] > 0:
-                print community_matrix[com, t]/comsizes[com], sum(tweets_matrix[:,t])/float(tweets_matrix.shape[0]), tweets_collection[t][0]['text'][14:] 
+    #2 imprime biclusters encontrados
+    tweets_collection = tm_dict['tweets_collection']
+    classes = tm_dict['classes']
+    
+    for bic in biclusters:
+        print "usuarios:", len(bic['A'][0]), "tweets:", len(bic['B'][0])
+        for c in bic['B'][0]:
+            i = int(c-1)
+            print classes[i], "\t", tweets_collection[i][0]['text'][14:] 
         print "\n"
-        
-    
-    
-    
-    
-
-        
-###############################################################################
-#biclusterizacao
-
-import numpy as np
-from matplotlib import pyplot as plt
-
-from sklearn.cluster.bicluster import SpectralCoclustering
 
 
-#data = tweets_matrix
-data = community_matrix
-
-plt.matshow(tweets_matrix_sparse.T, cmap=plt.cm.Blues)
-plt.title("Sparse dataset")
-
-plt.matshow(tweets_matrix.T, cmap=plt.cm.Blues)
-plt.title("Original dataset")
-
-plt.matshow(data.T, cmap=plt.cm.Blues)
-plt.title("Communities dataset")
-
-
-model = SpectralCoclustering(n_clusters=5)
-model.fit(data)
-
-
-fit_data = data[np.argsort(model.row_labels_)]
-fit_data = fit_data[:, np.argsort(model.column_labels_)]
-
-plt.matshow(fit_data.T, cmap=plt.cm.Blues)
-plt.title("After biclustering; rearranged to show biclusters")
-
-plt.show() 
-
-
-
-##bimax  
-
-import bibench.all as bb
-bb.heatmap(data)
-
-import bibench.all as bb         # import most commonly used functions
-data = bb.get_gds_data('GDS181') # download gene expression dataset GDS181
-data = bb.pca_impute(data)       # impute missing values
-biclusters = bb.plaid(data)      # cluster with Plaid algorithm
-
-row_labels = []
-column_labels = []
-for bic in biclusters:
-    row_labels += bic.rows
-    column_labels += bic.cols
-    
-
-fit_data = data[row_labels]
-fit_data = fit_data[:, column_labels]
-
-plt.matshow(fit_data.T, cmap=plt.cm.Blues)
-plt.title("After biclustering; rearranged to show biclusters")
-
-
-#imprime clusters
-for bic in biclusters:
-    for c in bic.cols:
-        print tweets_collection[c][0]['text'] 
-    print "\n\n"
-    
-    
-
-    
-    
-    
-##############################
-#regressao linear
-Y = np.array([class_dict[classe] for classe in classes])
-YY = list(Y)
-random.shuffle(YY)
-Y = np.array(YY)
-
-X = tweets_matrix.T
-
-
-total_size = Y.size
-random_indices = np.random.permutation(total_size)
-
-
-train_limit = 0.5
-Y_train = Y[:int(train_limit * total_size)]
-X_train = X[:int(train_limit * total_size)]
-
-Y_test = Y[int(train_limit * total_size):]
-X_test = X[int(train_limit * total_size):]
-
-
-teta = np.dot(np.linalg.pinv(X_train), Y_train)
-    
-Y_l = np.around(np.dot(X_test, teta))
-
-Y_ll = np.around(np.dot(X_train, teta))
-
-acertos = sum(Y_test == Y_l)
-
-
-
-#svm
-from sklearn import svm
-
-classificador = svm.SVC(kernel='linear', tol=2.0000000000000001)
-classificador.fit(X_train, Y_train)
-
-
-Y_classificado = classificador.predict(X_test)
-
-
-acertos = sum(Y_test == Y_classificado) / float(len(Y_classificado))
-
-Y_class2 = classificador.predict(X_train)
-
-acertos2 = sum(Y_train == Y_class2) / float(len(Y_class2))
-
-print acertos2, acertos
-
-
-#TODO: LDA
-
-
-
+#TODO: ver similaridades entre comunidades e biclusters
 
 
 ###############################################################################
@@ -571,9 +554,9 @@ for cat in class_dict.keys():
 #topicos diferentes por usuario
 
 
-#considera apenas usuarios com 5 ou mais tweets
+#considera apenas usuarios com 6 ou mais tweets
 tweetssum = np.sum(tweets_matrix, axis=1)
-topusers = np.where(tweetssum>=5)[0]
+topusers = np.where(tweetssum>=6)[0]
 
 
 bcount = []
@@ -585,7 +568,73 @@ for usr in tweets_matrix[topusers]:
 np.bincount(bcount)
 
     
-    
+###############################################################################
 
-   
-    
+
+#TODO:
+#PCA
+#mapa de distribuicoes
+#LDA?
+#redes neurais
+
+
+
+
+
+#obsoletos
+
+#from sklearn.cluster.bicluster import SpectralCoclustering
+#
+##data = community_matrix
+#data = TM_dict['TM_pop']['tweets_matrix'].
+#plt.matshow(TM_dict['TM_pop']['tweets_matrix'].T, cmap=plt.cm.Blues)
+#plt.title("Sparse dataset")
+#
+#plt.matshow(TM_dict['TM_pop_prop']['tweets_matrix'].T, cmap=plt.cm.Blues)
+#plt.title("Original dataset")
+#
+#plt.matshow(data.T, cmap=plt.cm.Blues)
+#plt.title("Communities dataset")
+#
+#
+#model = SpectralCoclustering(n_clusters=5)
+#model.fit(data)
+#
+#
+#fit_data = data[np.argsort(model.row_labels_)]
+#fit_data = fit_data[:, np.argsort(model.column_labels_)]
+#
+#plt.matshow(fit_data.T, cmap=plt.cm.Blues)
+#plt.title("After biclustering; rearranged to show biclusters")
+#
+#plt.show() 
+
+##bimax  
+
+#import bibench.all as bb
+#bb.heatmap(data)
+#
+#import bibench.all as bb         # import most commonly used functions
+#data = bb.get_gds_data('GDS181') # download gene expression dataset GDS181
+#data = bb.pca_impute(data)       # impute missing values
+#biclusters = bb.plaid(data)      # cluster with Plaid algorithm
+#
+#row_labels = []
+#column_labels = []
+#for bic in biclusters:
+#    row_labels += bic.rows
+#    column_labels += bic.cols
+#    
+#
+#fit_data = data[row_labels]
+#fit_data = fit_data[:, column_labels]
+#
+#plt.matshow(fit_data.T, cmap=plt.cm.Blues)
+#plt.title("After biclustering; rearranged to show biclusters")
+#
+#
+##imprime clusters
+#for bic in biclusters:
+#    for c in bic.cols:
+#        print tweets_collection[c][0]['text'] 
+#    print "\n\n" 
